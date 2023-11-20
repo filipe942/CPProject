a74e0d4025d4916afcd9fd83663177b0b963e3a2
JFreitas715 <jfreitascsm@hotmail.com>
2023-11-20 15:34:40 +0000

    more updates

--- Assets/DungeonKit/Scripts/Managers/EnemyManager.cs
+++ Assets/DungeonKit/Scripts/Managers/EnemyManager.cs
@@ -6,6 +6,7 @@ namespace DungeonKIT
     public class EnemyManager : MonoBehaviour
     {
         GameObject[] enemies; // Declare the array
+        GameObject[] boss;
 
         void Start()
         {
@@ -16,8 +17,10 @@ namespace DungeonKIT
         {
             // Find all game objects of a specific prefab
             enemies = GameObject.FindGameObjectsWithTag("Enemy"); // Replace with your enemy tag
+            boss= GameObject.FindGameObjectsWithTag("Boss");
             int enemyCount = enemies.Length;
-            Debug.Log("Total Enemies: " + enemyCount);
+            int bossCount=boss.Length;
+            Debug.Log("Total Enemies: " + enemyCount +". Total bosses: "+bossCount);
 
             if (enemyCount == 0)
             {
